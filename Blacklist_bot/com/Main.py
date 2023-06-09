8d0a007564428794ac80ce443bda256ba0762921
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-06-09 17:59:26 +0200

    Checking if command starts with '/' has been added

--- Blacklist_bot/com/Main.py
+++ Blacklist_bot/com/Main.py
@@ -2,7 +2,6 @@ import discord
 import os
 from MessageHandler import Handler
 
-
 DISCORD_TOKEN = os.getenv('DiscordToken')
 SERVER_CHANNEL_NAME = 'ogólny'
 
@@ -22,18 +21,27 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+    if not message.content.startswith('/'):
+        response = "Sorry but command: '{}' doesn't start at '/'".format(message.content)
+        await message.channel.send(response)
+        return
+
     if Handler.is_private_channel(message):
-        await message.channel.send('Private channel!')
+        await message.channel.send("Private channel!")
+        return
     else:
         if message.channel.name == SERVER_CHANNEL_NAME:
             if message.content == '$hello':
                 await message.channel.send('Hello!')
+                return
             else:
                 response = "Sorry, I dont know what is '{}'."
                 await message.channel.send(response.format(message.content))
+                return
         else:
             response = "Sorry, I can only answer on channel {} or private messages."
             await message.channel.send(response.format(SERVER_CHANNEL_NAME))
+            return 
 
 
 client.run(DISCORD_TOKEN)
