b728cc0c67f06e52b108e74f90016c3dd9cc7a45
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-06-20 23:00:08 +0200

    Code has been refactored

--- Blacklist_bot/com/services/PublicService.py
+++ Blacklist_bot/com/services/PublicService.py
@@ -1,5 +1,5 @@
 from enum import Enum
-import FileService
+from com.services import FileService
 
 
 class PublicCommands(Enum):
@@ -10,17 +10,14 @@ async def process_command(message, channel_name):
     split_message = message.content.split(' ')
 
     if len(split_message) != 2:
-        response_message = ":x: Sorry I accept only 2 parameters but passed {}. \n\n" \
-                           ":arrow_forward: Input i have received: {}."
-        await message.channel.send(response_message.format(len(split_message), message.content))
+        response_message = ":x: Sorry I accept only 2 parameters but passed {}. \n\n"
+        await message.channel.send(response_message.format(len(split_message)))
         return
 
     command = split_message[0]
     username = split_message[1]
 
     if command == PublicCommands.CHECK.value:
-
-
         await message.channel.send('Hello!')
         return
     else:
