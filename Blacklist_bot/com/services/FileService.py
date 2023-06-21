9debe08c3b355c4120be5a9d57e9fd1bc2cc5c27
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-06-21 19:54:33 +0200

    Added code that removes emoji, added method to update player

--- Blacklist_bot/com/services/FileService.py
+++ Blacklist_bot/com/services/FileService.py
@@ -8,11 +8,7 @@ import os
 def add_user_to_bl(who_added, username_to_bl, description):
     file = open(bl_path, mode="a")
     if os.path.exists(bl_path):
-        line_to_write = s.safe_string(username_to_bl) + "," + \
-                        s.safe_string(description).replace(",", " ").replace("  ", " ") + "," \
-                        + datetime.strptime(str(date.today()), "%Y-%m-%d").strftime('%d/%m/%Y') + "," \
-                        + s.safe_string(who_added) + "\n"
-        file.write(line_to_write)
+        file.write(prepare_line_to_write(who_added, username_to_bl, description))
         file.close()
         return True
     else:
@@ -42,3 +38,24 @@ def get_user_data(username_to_check):
         return ""
     else:
         return ""
+
+
+def update_user_data(who_updated, username, description):
+    if os.path.exists(bl_path):
+        with open(bl_path, "r") as file:
+            all_lines = file.readlines()
+        with open(bl_path, "w") as file:
+            for line in all_lines:
+                if s.contains(line.strip("\n"), username):
+                    file.write(prepare_line_to_write(who_updated, username, description))
+                    return True
+    else:
+        return False
+
+
+def prepare_line_to_write(who_prepared, username, description):
+    description = s.safe_string(description).replace(",", " ").replace("  ", " ")
+    username = s.safe_string(username)
+    line_date = datetime.strptime(str(date.today()), "%Y-%m-%d").strftime('%d/%m/%Y')
+    who_prepared = s.safe_string(who_prepared)
+    return username + "," + description + "," + line_date + "," + who_prepared + "\n"
