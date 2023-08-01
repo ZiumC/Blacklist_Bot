54115019d7e0c4dd121581649c1f6705c1e15f1c
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-08-01 15:32:28 +0200

    Command for checking last added user has been added

--- Blacklist_bot/src/config.py
+++ Blacklist_bot/src/config.py
@@ -9,14 +9,13 @@ MAX_MODERATION_COMMAND_LENGTH: Final[int] = 2
 MAX_PUBLIC_COMMAND_LENGTH: Final[int] = 2
 
 # Strongly recommended is to modify this configuration strings
-ADMINISTRATIVE_ROLE: Final[str] = ''
-PUBLIC_CHANNEL_BL: Final[str] = ''
-MODERATION_CHANNEL_BL: Final[str] = ''
-EXCEPTION_RESPONSE: Final[str] = ''
-GUILD_ID: Final[int] = 0
-DISCORD_TOKEN: Final[str] = ''
+ADMINISTRATIVE_ROLE: Final[str] = 'nowa rola'
+PUBLIC_CHANNEL_BL: Final[str] = 'test-public'
+MODERATION_CHANNEL_BL: Final[str] = 'test-moderacja'
+EXCEPTION_RESPONSE: Final[str] = 'Exception'
+GUILD_ID: Final[int] = int(os.getenv('DiscordGuildId'))
+DISCORD_TOKEN: Final[str] = os.getenv('DiscordToken')
 
 # Set paths to bod data files
-PATH_TO_BLOCKED_USERS_FILE: Final[str] = ''
-PATH_TO_LOG_FILE: Final[str] = ''
-
+PATH_TO_BLOCKED_USERS_FILE: Final[str] = os.getenv('DiscordBlPath')
+PATH_TO_LOG_FILE: Final[str] = os.getenv('DiscordLogFilePath')
